e8154a5fb559ca4b6f80e17366b792d917c34d0f
RanchAgon <agonranch981@gmail.com>
2024-11-14 10:59:11 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	 cout << "Our Lady Of Lourdes College Foundation" << endl;
+	 cout << "Computer Science Technology" << endl;
+	 cout << "Bachelor Science in Information Technology" << endl;
+	 cout << "Ranch Ryan C. Agon" << endl;
+	 cout << "P-2, Brgy. Calabasa, Labo Camarines Norte" << endl;
+	 return 0;
+	 
+}
